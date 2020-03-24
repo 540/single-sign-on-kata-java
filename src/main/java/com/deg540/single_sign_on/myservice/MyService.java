95dce3f8eba651add9b230b64b819fac4ea258ee
Gorka <gorka@540deg.com>
2020-03-24 11:49:56 +0100

    Improve single sign on kata

--- src/main/java/com/deg540/single_sign_on/myservice/MyService.java
+++ src/main/java/com/deg540/single_sign_on/myservice/MyService.java
@@ -2,6 +2,7 @@ package com.deg540.single_sign_on.myservice;
 
 import com.deg540.single_sign_on.sso.Request;
 import com.deg540.single_sign_on.sso.Response;
+import com.deg540.single_sign_on.sso.SSOToken;
 import com.deg540.single_sign_on.sso.SingleSignOnRegistry;
 
 public class MyService {
@@ -15,5 +16,14 @@ public class MyService {
     public Response handleRequest(Request request) {
         // TODO: check request has a valid SSOToken
         return new Response("hello " + request.getName() + "!");
+    }
+
+    public SSOToken handleRegister(String username, String password) {
+        // TODO: register and return token
+        return new SSOToken();
+    }
+
+    public void handleUnRegister(SSOToken token) {
+        // TODO: unregister token
     }
 }
